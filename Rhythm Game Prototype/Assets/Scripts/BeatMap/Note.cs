3a54d1963974f22fc17ab4b2488c550609d7b491
Joseph Collard <jcollard@chadwickschool.org>
2021-06-25 11:16:22 -0700

    Implemented Equals and HashCode to ensure duplicate notes are not added at the same beat.

--- Rhythm Game Prototype/Assets/Scripts/BeatMap/Note.cs
+++ Rhythm Game Prototype/Assets/Scripts/BeatMap/Note.cs
@@ -14,6 +14,21 @@ public class Note
     {
         this.input = input;
     }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Note note &&
+               type == note.type &&
+               input == note.input;
+    }
+
+    public override int GetHashCode()
+    {
+        int hashCode = -68311600;
+        hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(type);
+        hashCode = hashCode * -1521134295 + input.GetHashCode();
+        return hashCode;
+    }
 }
 
 public class ScratchNote : Note
@@ -30,6 +45,25 @@ public class DoubleNote : Note
         this.input2 = second;
         this.type = "Double";
     }
+
+    public override bool Equals(object obj)
+    {
+        return obj is DoubleNote note &&
+               base.Equals(obj) &&
+               type == note.type &&
+               input == note.input &&
+               input2 == note.input2;
+    }
+
+    public override int GetHashCode()
+    {
+        int hashCode = 251041073;
+        hashCode = hashCode * -1521134295 + base.GetHashCode();
+        hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(type);
+        hashCode = hashCode * -1521134295 + input.GetHashCode();
+        hashCode = hashCode * -1521134295 + input2.GetHashCode();
+        return hashCode;
+    }
 }
 
 public class HoldNote : Note
@@ -40,5 +74,24 @@ public class HoldNote : Note
     {
         this.duration = duration;
         this.type = "Hold";
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is HoldNote note &&
+               base.Equals(obj) &&
+               type == note.type &&
+               input == note.input &&
+               duration == note.duration;
+    }
+
+    public override int GetHashCode()
+    {
+        int hashCode = -1512021421;
+        hashCode = hashCode * -1521134295 + base.GetHashCode();
+        hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(type);
+        hashCode = hashCode * -1521134295 + input.GetHashCode();
+        hashCode = hashCode * -1521134295 + duration.GetHashCode();
+        return hashCode;
     }
 }
